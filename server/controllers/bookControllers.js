76c9548c0669e3768afdd73ac1a8e2d129535ee7
wechuli <wechulipaul@yahoo.com>
2019-01-09 09:57:46 +0300

    finishing the book controller for basic CRUD actions

--- server/controllers/bookControllers.js
+++ server/controllers/bookControllers.js
@@ -18,31 +18,168 @@ module.exports = {
     }
   },
   singleBook: async (req, res) => {
-    res.status(200).json({ message: "Get a single book" });
+    const { bookId } = req.params;
+    try {
+      const book = await Book.findById(bookId)
+        .populate("author")
+        .populate("category")
+        .populate("instances")
+        .populate("reviews")
+        .exec();
+      if (book) {
+        res.status(200).json({ message: "Get a single book" });
+      } else {
+        res.status(404).json({ error: "Book not found" });
+      }
+    } catch (error) {
+      res.status(500).json(error);
+    }
   },
   allBookInstances: async (req, res) => {
-    res.status(200).json({ message: "Get all instances of a book" });
+    //this is a useless route,I could just have gotten the instance from the individual book route
+    const { bookId } = req.params;
+    try {
+      const instances = Book.findById(bookId)
+        .select("title instances")
+        .populate("instances")
+        .exec();
+      if (instances) {
+        res.status(200).json({ instances });
+      } else {
+        res
+          .status(404)
+          .json({ error: "No instances found or Incorrect book Id" });
+      }
+    } catch (error) {
+      res.status(500).json(error);
+    }
   },
   allAuthors: async (req, res) => {
-    res.status(200).json({ message: "Get all authors" });
+    try {
+      const allAuthors = Author.find({})
+        .populate("books")
+        .exec();
+      res.status(200).json({ allAuthors });
+    } catch (error) {
+      res.status(500).json(error);
+    }
   },
   singleAuthor: async (req, res) => {
+    const { authorId } = req.params;
     try {
-      const { authorId } = req.params;
       const author = await Author.findById(authorId)
         .populate({
           path: "books"
         })
         .exec();
-      res.status(200).json({ author });
+      if (author) {
+        res.status(200).json({ author });
+      } else {
+        res.status(404).json({ error: "Author not Found" });
+      }
     } catch (error) {
       res.status(500).json({ error });
     }
   },
   allBookReviews: async (req, res) => {
-    res.status(200).json({ message: "Get all reviews of a book" });
+    //another useless route -looks like i like doing this
+    const { bookId } = req.params;
+    try {
+      const book = Book.findById(bookId)
+        .populate("reviews")
+        .exec();
+      if (book) {
+        res.status(200).json({ book });
+      } else {
+        res.status(404).json({ error: "Invalid book Id" });
+      }
+    } catch (error) {
+      res.status(500).json(error);
+    }
   },
   reviewBook: async (req, res) => {
-    res.status(200).json({ message: "Review a single book" });
+    //we should have a user object from the request since only authenticated user will be allowed access to this route
+    const { bookId } = req.params;
+    try {
+      const book = Book.findById(bookId);
+      if (book) {
+        const newReview = new Review(req.body);
+
+        newReview.book = book;
+        newReview.author = req.User;
+        newReview.date_created = Date.now();
+        await newReview.save();
+        //add the review to the user object
+        const user = await User.findById(req.User._id);
+        user.reviews.push(newReview);
+        await user.save();
+        //add this review to the book object
+        book.reviews.push(newReview);
+        await book.save();
+        res.status(201).json({ message: "Review Added", newReview });
+      } else {
+        res.status(404).json({ error: "Invalid bookId " });
+      }
+    } catch (error) {
+      res.status(500).json(error);
+    }
+  },
+
+  //Check below last 09/01/2019-below code is wrong wrong wrong
+
+  
+  editReview: async (req, res) => {
+     //we should have a user object from the request since only authenticated user will be allowed access to this route
+     const { bookId } = req.params;
+     try {
+       const book = Book.findById(bookId);
+       if (book) {
+         const newReview = new Review(req.body);
+ 
+         newReview.book = book;
+         newReview.author = req.User;
+         newReview.date_created = Date.now();
+         await newReview.save();
+         //add the review to the user object
+         const user = await User.findById(req.User._id);
+         user.reviews.push(newReview);
+         await user.save();
+         //add this review to the book object
+         book.reviews.push(newReview);
+         await book.save();
+         res.status(201).json({ message: "Review Added", newReview });
+       } else {
+         res.status(404).json({ error: "Invalid bookId " });
+       }
+     } catch (error) {
+       res.status(500).json(error);
+     }
+  },
+  deleteReview = async (req,res)=>{
+     //we should have a user object from the request since only authenticated user will be allowed access to this route
+     const { bookId } = req.params;
+     try {
+       const book = Book.findById(bookId);
+       if (book) {
+         const newReview = new Review(req.body);
+ 
+         newReview.book = book;
+         newReview.author = req.User;
+         newReview.date_created = Date.now();
+         await newReview.save();
+         //add the review to the user object
+         const user = await User.findById(req.User._id);
+         user.reviews.push(newReview);
+         await user.save();
+         //add this review to the book object
+         book.reviews.push(newReview);
+         await book.save();
+         res.status(201).json({ message: "Review Added", newReview });
+       } else {
+         res.status(404).json({ error: "Invalid bookId " });
+       }
+     } catch (error) {
+       res.status(500).json(error);
+     }
   }
 };
--- server/routes/bookRoutes.js
+++ server/routes/bookRoutes.js
@@ -8,7 +8,11 @@ router.get("/book/:bookId", bookControllers.singleBook); //get a particular book
 router.get("/bookinstances/:bookId", bookControllers.allBookInstances); //get all instances of a book
 router.get("/allauthors", bookControllers.allAuthors); //get all authors in the library
 router.get("/author/:authorId", bookControllers.singleAuthor); //get a single author
-router.get('/book/reviews/:bookId',bookControllers.allBookReviews); //get all reviews of a particular book, unauthenticated users can get book reviews too
-router.post('/book/review/:bookId',bookControllers.reviewBook); //review a book, only logged in users can review a book
+router.get("/book/reviews/:bookId", bookControllers.allBookReviews); //get all reviews of a particular book, unauthenticated users can get book reviews too
+router
+  .route("/book/review/:bookId")
+  .post(bookControllers.reviewBook)
+  .put(bookControllers.editReview)
+  .delete(bookControllers.deleteReview); //review a book, only logged in users can review a book
 
 module.exports = router;
