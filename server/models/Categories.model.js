fa9022c0238db826ae005c9ada3cb0cedea0abb7
Wechuli <wechulipaul@yahoo.com>
2019-05-05 21:04:05 +0300

    re-do server api first

--- server/models/Categories.model.js
+++
@@ -1,9 +0,0 @@
-const mongoose = require("mongoose");
-
-const Schema = mongoose.Schema;
-
-const categoriesSchema = new Schema({
-  name: String
-});
-
-module.exports = mongoose.model("category", categoriesSchema);
