a5088af8bdb6f5e038ea3148a69c2c8a69fc5c32
Wechuli <wechulipaul@yahoo.com>
2019-05-11 12:40:03 +0300

    author routes, controllers and validation

---
+++ server/controllers/authorController.js
@@ -0,0 +1,61 @@
+const Author = require('../models/Author.model');
+
+
+
+module.exports = {
+
+    //create a new author
+    async createAuthor(req,res){
+try {
+    const newAuthor = new Author(req.body);
+    
+
+    
+} catch (error) {
+    res.status(500).json(error);
+}
+    },
+
+    //edit an existing author
+    async editAuthor(req,res){
+
+        try {
+    
+        } catch (error) {
+            res.status(500).json(error);
+        }
+    },
+
+    //delete an existing author
+    async deleteAuthor(req,res){
+
+
+        try {
+    
+        } catch (error) {
+            res.status(500).json(error);
+        }
+    },
+
+    //get all authors in the system
+    async getAllAuthors(req,res){
+
+
+        try {
+    
+        } catch (error) {
+            res.status(500).json(error);
+        }
+    },
+
+    //get a single author
+    async getAuthor(req,res){
+
+
+        try {
+    
+        } catch (error) {
+            res.status(500).json(error);
+        }
+    }
+}
--- server/routes/authorRoutes.js
+++ server/routes/authorRoutes.js
@@ -1,10 +1,27 @@
-const express = require('express');
+const express = require("express");
+const {
+  createAuthor,
+  deleteAuthor,
+  editAuthor,
+  getAllAuthors,
+  getAuthor
+} = require("../controllers/authorController");
 
 const router = express.Router();
 
+//create a new author - authenticated
+router.post("/create", createAuthor);
 
+//delete an author  - authenticated
+router.delete("/delete/:authorId", deleteAuthor);
 
+//get a single author   -unauthenticated
+router.get("/single/:authorId", getAuthor);
 
+//get all authors - unauthenticated
+router.get("/all", getAllAuthors);
 
+//edit an author
+router.patch("/edit/:authorId", editAuthor);
 
 module.exports = router;
